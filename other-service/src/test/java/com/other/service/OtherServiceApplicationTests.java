70af55b795e886319ac6befe64cf824f19844e69
“claudio-code” <suporte2424@outlook.com>
2022-09-25 19:22:28 -0300

    Create other service with mysql

---
+++ other-service/src/test/java/com/other/service/OtherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
